51687a20416bb78b963895fe13388f8758a000ed
choeminseob <minsub0922@naver.com>
2020-01-11 13:39:54 +0900

    Kakao Login API Sync

--- MNT_iOS/AppDelegate.swift
+++ MNT_iOS/AppDelegate.swift
@@ -12,21 +12,47 @@ import CoreData
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
 
-
+    // MARK:- APP Lifecycle
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow()
+        window?.rootViewController = LoginController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        //카카오 로그인 페이지만 보여주는 것. 토큰 처리는 따로 필요
+        if KOSession.isKakaoAgeAuthCallback(url.absoluteURL) {
+            return KOSession.handleOpen(url)
+        }
+        
+        return true
+    }
+    
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        //카카오 로그인 페이지만 보여주는 것. 토큰 처리는 따로 필요
+        if KOSession.isKakaoAgeAuthCallback(url.absoluteURL) {
+            return KOSession.handleOpen(url)
+        }
+        
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
--- MNT_iOS/ViewController.swift
+++ MNT_iOS/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let emailLabel: UILabel = {
+        let lb = UILabel()
+        return lb
+    }()
+    let nicknameLabel: UILabel = {
+        let lb = UILabel()
+        return lb
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    fileprivate func setupLayout() {
+        view.addSubview(emailLabel)
+        view.addSubview(nicknameLabel)
+        
+        NSLayoutConstraint.activate([
+            emailLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            emailLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            nicknameLabel.topAnchor.constraint(equalTo: emailLabel.bottomAnchor, constant: 20),
+            nicknameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
 }
 
