60c97e7e1b1b7fc40bb39fe546f0acd371d9498b
한지혜 <zzlhye0307@gmail.com>
2020-02-16 16:20:51 +0900

    Re-Create RoomModel

---
+++ MNT_iOS/Main/Model/Room.swift
@@ -0,0 +1,28 @@
+//
+//  Room.swift
+//  MNT_iOS
+//
+//  Created by Jihye on 2020/02/16.
+//  Copyright © 2020 최민섭. All rights reserved.
+//
+
+import Foundation
+
+struct RoomAttendResponse: Codable {
+    let apiStatus: APIStatus
+    let data: Room
+}
+
+struct Room: Codable, getRoomViewModel {
+    let endDay: String
+    let id: Int
+    let isDone: Int
+    let isStart: Int
+    let maxPeople: Int
+    let name: String
+    let startDay: String
+    
+    var asRoomViewModel: RoomViewModel {
+        return RoomViewModel(datas: self)
+    }
+}
